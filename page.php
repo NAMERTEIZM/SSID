75edaa466ca9ad79171c4326f7756628167fd45f
NAMERTEIZM <109963581+NAMERTEIZM@users.noreply.github.com>
2022-07-25 15:15:21 +0300

    Add files via upload

---
+++ page.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <title>SSID Deneme</title>
+</head>
+<body>
+<div class="container" id="login-container" >
+<div class="row justify-content-center">
+
+    
+<div id="row1" class="col-12"  >
+<div class="daireclass" id="logo">
+<img id="resim" src="img/layer-3@3x.png">
+</div>
+    </div>
+
+   <div id="row2" class="col-12" >
+   Web Sayfamızı Sizler için Yeniliyoruz...
+
+   </div>
+
+   <div id="row3" class="col-12" >
+  <span id="adres">Mimar Sinan Mah. Mimar Sinan Cd. Toplusoy İş Merkezi,<p></p>
+             No:5 D:3, 34782 Çekmeköy/İstanbul </span> 
+   </div>
+
+   <div id="row4" class="col-12 justify-content-between align-items-center "  >
+   02166390603      <img id="phone" src="img/phone@3x.png">             |             <img id="phone" src="img/mail-2@3x.png">      info@ssidglobal.com     
+   </div>
+
+   
+   <div id="row5" class="col-12" >
+   
+   <div id="fot1" class="col-3"> 
+   
+   <img id="bee" src="img/bee.png">  
+   </div>
+ 
+   
+   <div id="fot2" class="col-3" >
+   
+   <img id="notice" src="img/notice.png">  
+   
+</div>
+   
+   <div id="fot3" class="col-3" >
+   
+   <img id="mach" src="img/mach.png">  
+   
+</div>
+   
+   <div id="fot4" class="col-3" >
+   <img id="dex" src="img/DEX.png">     
+
+
+</div>
+</div>
+   
+</div>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
